c61b447035db99493b0e84b382c66317c489be99
hiroki0116 <hirokiseino0116@gmail.com>
2025-05-18 12:38:41 +0900

    認証moduleの関心の分離

--- packages/backend/src/auth/auth.module.ts
+++ packages/backend/src/auth/auth.module.ts
@@ -2,8 +2,19 @@
 
 import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
-import { AuthController } from "src/presentation/controllers/auth.controller";
-import { PrismaModule } from "src/prisma/prisma.module";
+import { AuthController } from "../presentation/controllers/auth.controller";
+import { PrismaModule } from "../prisma/prisma.module";
+import { JwtStrategy } from "../infrastructure/authentication/strategies/jwt.strategy";
+import { JwtAuthGuard } from "../infrastructure/authentication/guards/jwt-auth.guard";
+import { JwtService } from "../infrastructure/authentication/jwt/jwt.service";
+import { APP_GUARD } from "@nestjs/core";
+import { Reflector } from "@nestjs/core";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { PrismaUserRepository } from "../infrastructure/repositories/prisma-user.repository";
+import { USER_REPOSITORY } from "../domain/user/repositories/user.reposiroty.interface";
+import { UserMapper } from "../infrastructure/mappers/user.mapper";
 
 @Module({
   imports: [
@@ -11,9 +22,36 @@ import { PrismaModule } from "src/prisma/prisma.module";
     CqrsModule,
     // Prismaを使用するためのモジュール
     PrismaModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>("JWT_SECRET"),
+        signOptions: {
+          expiresIn: configService.get<string>("JWT_EXPIRES_IN") || "1d",
+        },
+      }),
+    }),
   ],
   controllers: [AuthController],
-  providers: [],
-  exports: [],
+  providers: [
+    JwtStrategy,
+    JwtAuthGuard,
+    JwtService,
+    UserMapper,
+    {
+      provide: USER_REPOSITORY,
+      useClass: PrismaUserRepository,
+    },
+    {
+      provide: APP_GUARD,
+      useFactory: (reflector) => {
+        return new JwtAuthGuard(reflector);
+      },
+      inject: [Reflector],
+    },
+  ],
+  exports: [JwtService],
 })
 export class AuthModule {}
